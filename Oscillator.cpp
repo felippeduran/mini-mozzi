4d2a5c1d2aa7873411a10fd7344a26d1f3ac748e
Felippe Durán <felippe.duran@tfgco.com>
2018-02-01 10:04:12 -0200

    Add range, note and detune to Oscillator.

--- Oscillator.cpp
+++ Oscillator.cpp
@@ -1,4 +1,5 @@
 #include "Oscillator.h"
+#include "mozzi_midi.h"
 
 int8_t Oscillator::next() {
 	int8_t signal = 0;
@@ -21,4 +22,41 @@ int8_t Oscillator::next() {
   }
 
 	return ((int16_t)gain * signal) >> 7;
+}
+
+void Oscillator::updateFrequency() {
+	int8_t midiNote = note + rangeModifier[range];
+	Q16n16 majorSixthFreq = Q16n16_mtof(Q8n0_to_Q16n16(midiNote + 9));
+	Q16n16 majorSixthLowFreq = Q16n16_mtof(Q8n0_to_Q16n16(midiNote - 3));
+	Q16n16 freq = majorSixthLowFreq + detune * ((majorSixthFreq - majorSixthLowFreq) / (uint16_t)1023);
+	aSaw.setFreq_Q16n16(freq);
+	aSquare.setFreq_Q16n16(freq);
+	aSin.setFreq_Q16n16(freq);
+	aTriangle.setFreq_Q16n16(freq);
+}
+
+void Oscillator::setNote(int8_t note) {
+	this->note = note;
+
+	// int8_t midiNote = note + rangeModifier[range];
+
+	// // Q16n16 freq = Q16n16_mtof(Q8n0_to_Q16n16(midiNote));
+	// Q16n16 majorSixthFreq = Q16n16_mtof(Q8n0_to_Q16n16(midiNote + 9));
+	// Q16n16 majorSixthLowFreq = Q16n16_mtof(Q8n0_to_Q16n16(midiNote - 3));
+
+	// Serial.print(Q16n0_to_Q16n16(detune));
+	// Serial.print(" ");
+	// Serial.print(Q16n0_to_Q16n16((uint16_t)1023));
+	// Serial.print(" ");
+	// Serial.print((majorSixthFreq - majorSixthLowFreq));
+	// Serial.print(" ");
+	// Serial.print((majorSixthFreq - majorSixthLowFreq) * Q16n0_to_Q16n16(detune));
+	// Serial.print(" ");
+
+	// Q16n16 freq = majorSixthLowFreq + detune * ((majorSixthFreq - majorSixthLowFreq) / (uint16_t)1023);
+	// aSaw.setFreq_Q16n16(freq);
+	// aSquare.setFreq_Q16n16(freq);
+	// aSin.setFreq_Q16n16(freq);
+	// aTriangle.setFreq_Q16n16(freq);
+	// Serial.println(Q16n16_to_float(freq));
 }
--- Oscillator.h
+++ Oscillator.h
@@ -9,15 +9,24 @@
 #include <tables/triangle2048_int8.h>
 
 struct Oscillator {
-    Oscillator() : waveform(Saw) {};
+    Oscillator() : waveform(Saw) {}; //193.55698098
 
-    typedef enum { Saw, Square, Sine, Triangle, Count } Waveform;
+    typedef enum { Saw, Square, Sine, Triangle, WaveCount } Waveform;
+    typedef enum { Two, Four, Eight, Sixteen, ThirthyTwo, RangeCount } Range;
 
     int8_t next();
+    void updateFrequency();
+
+    void setNote(int8_t note);
+
+    int8_t rangeModifier[5] = { 89, 77, 65, 53, 41 };
 
     Waveform waveform;
+    Range range = 4;
     bool enabled;
+    int8_t note;
     int8_t gain;
+    uint16_t detune = 193;
     Oscil<SIN2048_NUM_CELLS, AUDIO_RATE> aSin = Oscil<SIN2048_NUM_CELLS, AUDIO_RATE>(SIN2048_DATA);
     Oscil<SQUARE_NO_ALIAS_2048_NUM_CELLS, AUDIO_RATE> aSquare = Oscil<SQUARE_NO_ALIAS_2048_NUM_CELLS, AUDIO_RATE>(SQUARE_NO_ALIAS_2048_DATA);
     Oscil<SAW2048_NUM_CELLS, AUDIO_RATE> aSaw = Oscil<SAW2048_NUM_CELLS, AUDIO_RATE>(SAW2048_DATA);
